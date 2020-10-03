554a794320129294df74ce282b420c760ab9c8fa
cosentinowebdev <48664024+cosentinowebdev@users.noreply.github.com>
2020-10-03 20:20:03 -0300

    creando select
    no encuentro la forma de acceder a los atributos del objeto de los array ni si indice

--- smnApp/src/app/app.module.ts
+++ smnApp/src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
 import { FormClimaComponent } from './form-clima/form-clima.component';
+import { AutocompletarComponent } from './autocompletar/autocompletar.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { SmnApiService } from './smn-api.service';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { AutocompletarComponent } from './autocompletar/autocompletar.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @NgModule({
@@ -19,7 +22,8 @@ import { AutocompletarComponent } from './autocompletar/autocompletar.component'
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatAutocompleteModule
+    MatAutocompleteModule,
+    MatFormFieldModule
 
   ],
   providers: [SmnApiService],
