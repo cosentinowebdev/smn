4d6d938aebbb1fa87dd0efc489b52ffe6719db46
cosentinowebdev <48664024+cosentinowebdev@users.noreply.github.com>
2020-10-07 00:41:46 -0300

    terminando los estilos

--- smnApp/src/app/app.module.ts
+++ smnApp/src/app/app.module.ts
@@ -2,31 +2,45 @@
 import { AppComponent } from './app.component';
 import { FormClimaComponent } from './form-clima/form-clima.component';
 import { AutocompletarComponent } from './autocompletar/autocompletar.component';
+import { SmnApiService } from './smn-api.service';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { SmnApiService } from './smn-api.service';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatListModule} from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule,Routes } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  {path: 'lista', component: FormClimaComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo:'home',pathMatch:'full'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     FormClimaComponent,
-    AutocompletarComponent
+    AutocompletarComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatCardModule,
     MatButtonModule,
-    MatListModule
+    MatListModule,
+    RouterModule.forRoot(appRoutes)
 
   ],
   providers: [SmnApiService],
